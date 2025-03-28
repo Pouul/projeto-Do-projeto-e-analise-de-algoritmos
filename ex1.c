b2d1b3c742fa23be0e65c23e6e5eb5561477e4aa
Pouul <paulohuang04@gmail.com>
2025-03-28 12:42:02 +0000

    ex1.2

--- ex1.c
+++ ex1.c
@@ -1,8 +1,11 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-int main(){
-    char *alfabeto;
-
-    
+int main(int argc, char *argv[]){
+    if (argc != 2) {
+        printf("Uso: %s <string>\n", argv[0]);
+        return 1;
+    }
+    char *entrada = argv[1];
+    char digito = entrada[0];
 }
